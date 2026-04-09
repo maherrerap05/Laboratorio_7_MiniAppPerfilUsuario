f5a467dd8a5fca238cf041beb84170a01083a2da
Martin Herrera <maherrerap@puce.edu.ec>
2026-04-08 19:44:12 -0500

    Initial Commit: Parte 6 - Crear la UI en Compose

--- app/src/main/java/com/example/lab/ui/user/UserScreen.kt
+++ app/src/main/java/com/example/lab/ui/user/UserScreen.kt
@@ -1,2 +1,51 @@
 package com.example.lab.ui.user
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.lab.data.User
+
+@Composable
+fun UserScreen(state:UiState) {
+    when(state){
+        is UiState.Loading -> LoadingView()
+        is UiState.Success -> UserView(state.user)
+        is UiState.Error -> ErrorView(state.message)
+    }
+}
+
+@Composable
+fun LoadingView() {
+    Box (
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        CircularProgressIndicator()
+    }
+}
+
+@Composable
+fun UserView(user: User) {
+    Column (
+        modifier = Modifier.fillMaxSize().padding(24.dp),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text("Usuario cargado.")
+        Spacer(Modifier.height(8.dp))
+        Text("ID: ${user.email}")
+    }
+}
+
+@Composable
+fun ErrorView(message: String) {
+    Box (
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text("Error: $message")
+    }
+}
+
