4128bff67adffe93c9343290aa934c7a6cbaa9d6
Martin Herrera <maherrerap@puce.edu.ec>
2026-04-08 19:17:41 -0500

    Initial Commit: Parte 5 - Crear el ViewModel con StateFlow

--- app/src/main/java/com/example/lab/ui/user/UserViewModel.kt
+++ app/src/main/java/com/example/lab/ui/user/UserViewModel.kt
@@ -1,2 +1,29 @@
 package com.example.lab.ui.user
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.lab.data.ApiClient
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class UserViewModel: ViewModel() {
+    private val _state = MutableStateFlow<UiState>(UiState.Loading)
+    val state = _state.asStateFlow()
+
+    init {
+        loadUser()
+    }
+
+    private fun loadUser() {
+        viewModelScope.launch {
+            try {
+                val user = ApiClient.fetchUser()
+                _state.value = UiState.Success(user)
+            } catch (e: Exception) {
+                _state.value = UiState.Error("No se pudo cargar el usuario.")
+            }
+        }
+    }
+}
+
