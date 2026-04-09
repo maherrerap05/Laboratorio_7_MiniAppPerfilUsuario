73dff64a5694ebb826eb953ee663920f2e49d3eb
Martin Herrera <maherrerap@puce.edu.ec>
2026-04-08 19:03:30 -0500

    Initial Commit: Parte 3 a Parte 4 - Crear Modelo Inmutable y Máquina de Estados

--- app/src/main/java/com/example/lab/ui/user/UiState.kt
+++ app/src/main/java/com/example/lab/ui/user/UiState.kt
@@ -1,2 +1,9 @@
 package com.example.lab.ui.user
 
+import com.example.lab.data.User
+
+sealed class UiState {
+    data object Loading: UiState()
+    data class Success(val user:User): UiState()
+    data class Error(val message:String): UiState()
+}
